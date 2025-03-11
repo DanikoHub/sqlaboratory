4506cc7485a69812d98659b614f460bd9da6b522
Даниил Боровков <194267398+DanikoHub@users.noreply.github.com>
2025-03-11 14:24:31 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# SQLaboratory
+
+## What is it?
+
+**SQLaboratory is a simple way to work with SQLAlchemy library**
+
+The idea behind is to write code like you would write SQL
+
+## How to use it
+### Select example in SQLab:
+**main.py**
+```python
+from sqlaboratory import SQLab
+from users import Users
+
+SQLab.connect("sqlite:///test.db")
+
+res = SQLab.select_from(Users, 
+	_select = Users.name, 
+	_where = Users.id < 3, 
+	_orderby = Users.id, 
+	_limit = 1)
+
+print(res)
+# [('John',)]
+```
+
+**users.py**
+```python
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String
+
+from sqlaboratory import SQLab, Base
+
+class Users(Base):
+	__tablename__ = "users"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	name: Mapped[str] = mapped_column(String, unique=True)
+
+	def __repr__(self) -> str:
+			return f"Users(id={self.id} name={self.name})\n\n"
+```
+
+### Create, Update, Delete:
+**main.py**
+```python
+from sqlab import SQLab
+from users import Users
+
+SQLab.connect("sqlite:///test.db")
+
+SQLab.create(Users(name = "Simon"))
+SQLab.update(Users, {"name" : "BetterSimon"}, _where = Users.name == "Simon")
+res = SQLab.select_from(Users)
+print(res)
+# [Users(id=1 name=John), Users(id=2 name=BetterSimon)]
+
+SQLab.delete(Users, _where = Users.name == "BetterSimon")
+res = SQLab.select_from(Users)
+print(res)
+# [Users(id=1 name=John)]
+```
+
+### Complex queries:
+**main.py**
+```python
+from sqlab import SQLab
+from users import Users
+
+SQLab.connect("sqlite:///test.db")
+
+SQLab.create(Users(name = "Simon")) # 2
+SQLab.create(Users(name = "Anne"))  # 3
+SQLab.create(Users(name = "Peter")) # 4
+SQLab.create(Users(name = "Kate"))  # 5
+SQLab.create(Users(name = "Alice")) # 6
+SQLab.create(Users(name = "Alex"))  # 7
+SQLab.create(Users(name = "Ben"))   # 8
+
+res = SQLab.select_from(
+	_from = Users,
+	_select = [Users.name, Users.id],
+	_where = (Users.id > 3) & ((Users.id.in_([4, 5]) | (Users.id == 7))),
+	_orderby = [Users.name, Users.id.desc()],
+	_limit = 3)
+print(res)
+# [('Alex', 7), ('Kate', 5), ('Peter', 4)]
+```
+
+### Get select query:
+**main.py**
+```python
+from sqlab import SQLab
+from users import Users
+
+SQLab.connect("sqlite:///test.db")
+
+res = SQLab.select_from(
+	_from = Users,
+	_limit = -1)
+print(res)
+# SELECT users.id AS users_id, users.name AS users_name 
+# FROM users
+```
+
+## What is missing
+
+- **JOINs**
+- **SQL functions**
+- **Table operations**
---
+++ sqlaboratory.py
@@ -0,0 +1,110 @@
+from typing import Any, Dict
+from sqlalchemy import create_engine, or_
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+	pass
+
+class SQLab:
+
+	db_url : str
+	engine = None
+	Session : sessionmaker
+	
+	@classmethod
+	def connect(cls, db_url):
+		cls.db_url = db_url
+		cls.engine = create_engine(db_url, poolclass=NullPool, pool_pre_ping=True)
+		cls.Session = sessionmaker(bind=cls.engine)()
+		Base.metadata.create_all(cls.engine)
+
+	@classmethod
+	def select_from(cls, _from : Base, _select : Base | Any = None, _where : Any = None, _orderby = None, _limit : int = None) -> Any | None:
+		try:
+			select = _select if isinstance(_select, list) or _select is None else [_select]
+			with cls.Session as session:
+				if select is not None:
+					result_query = session.query(*select)
+				else:
+					result_query = session.query(_from)
+			
+			if _where is not None:
+				result_query = result_query.filter(_where)
+
+			if _orderby is not None:
+				if not isinstance(_orderby, list):
+					orderby = [_orderby]
+				else:
+					orderby = _orderby
+				
+				if len(orderby) > 0:
+					result_query = result_query.order_by(*orderby)
+
+			if _limit == None:
+				return result_query.all()
+			elif _limit == -1:
+				return result_query
+			
+			return result_query.limit(_limit).all()
+
+		except Exception as e:
+			print("select - ", e)
+			return None
+
+	@classmethod
+	def create(cls, new_object : Base) -> None:
+		try:
+			unique_fields = []
+			for field in new_object.__table__.__dict__["_columns"]:
+				if field.primary_key or field.unique:
+					unique_fields.append(field)
+
+			check_object_in_db = cls.select_from(
+				_from = new_object.__table__, 
+				_where = or_(field == getattr(new_object,field.name) for field in unique_fields)
+			)
+
+			if check_object_in_db == []:
+				with cls.Session as session:
+					try:
+						session.add(new_object)
+
+					except Exception as e:
+						session.rollback()
+
+					else:
+						session.commit()
+
+		except Exception as e:
+			print("create - ", e)
+
+	@classmethod
+	def update(cls, table_name : Base, new_values : Dict[str, Any], _where : Any) -> None:
+		with cls.Session as session:
+			try:
+				objects_to_update = cls.select_from(_from=table_name, _where=_where, _limit=-1)
+				objects_to_update.update(new_values)
+
+			except Exception as e:
+				print("update - ", e)
+				session.rollback()
+
+			else:
+				session.commit()
+
+
+	@classmethod
+	def delete(cls, table_name : Base, _where : Any = None) -> None:
+		with cls.Session as session:
+			try:
+				objects_to_delete = cls.select_from(_from = table_name, _where=_where, _limit=-1)
+				objects_to_delete.delete()
+
+			except Exception as e:
+				print("delete - ", e)
+				session.rollback()
+
+			else:
+				session.commit()
